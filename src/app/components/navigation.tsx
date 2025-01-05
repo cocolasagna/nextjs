6dab340110ef88788e3f799a7620751c739f5d0c
cocolasagna <msujan795@gmail.com>
2025-01-05 17:01:34 +0545

    learning routing and restful api in nextjs

---
+++ src/app/components/navigation.tsx
@@ -0,0 +1,13 @@
+"use client"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+export const Navigation = ()=>{
+    const pathname = usePathname()
+    return (
+        <nav>
+        <Link href = '/' className={pathname === '/'? "font-bold mr-4":"text-blue-500 mr-4"}>Home</Link>
+        <Link href = '/about' className={pathname === '/about'?'font-bold mr-4':'mr-4 text-blue-500'}>About</Link>
+        <Link href = '/products/sujan' className='mr-4 text-blue-500'>Product</Link>
+        </nav>
+    )
+}
